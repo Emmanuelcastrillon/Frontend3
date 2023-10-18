f56910c664ed21f1c0e39c75e10909497e22f54f
unknown <castrillon.rivas@gmail.com>
2023-10-17 21:47:56 -0500

    clase3

---
+++ miPrimerProyectoVite/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Saludo from './Components/Saludo'
+
+
+function App() {
+  
+const estudiante='Emmanuel'
+
+  return (
+    <>
+     <Saludo nombre={estudiante}/>
+    </>
+    
+    
+  )
+}
+
+export default App
---
+++ miPrimerProyectoVite/src/Components/Saludo.jsx
@@ -0,0 +1,12 @@
+
+const Saludo = (props) => {
+  return (
+    <div>
+      {/* eslint-disable-next-line react/prop-types */}
+        <h1>Hola: {props.nombre}</h1>
+        
+    </div>
+  )
+}
+
+export default Saludo
